ae7ec71e5b177ec9082f2828be3bf30f4ef24c3d
Ken760 <49761163+Ken760@users.noreply.github.com>
2020-12-17 13:04:40 +0300

    Update locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -6,4 +6,11 @@ class YandexSearchLocators:
     SUGGEST = (By.CLASS_NAME, "mini-suggest__popup_visible")
     YANDEX_SEARCH_BUTTON = (By.CLASS_NAME, "search2__button")
     SEARCH_RESULTS = (By.CLASS_NAME, "organic typo typo_text_m typo_line_s i-bem")
+
+
+class YandexPictureLocators:
     YANDEX_IMAGE_LINK = (By.XPATH, "//a[@data-id='images']")
+    SELECTING_IMAGES_THEME = (By.CLASS_NAME, "PopularRequestList - Thumb")
+    SELECTING_IMAGES = (By.CLASS_NAME, "serp-item__link")
+    NEXT_IMAGE_BUTTON = (By.CLASS_NAME, "CircleButton_type_next")
+    PREVIOUS_IMAGE_BUTTON = (By.CLASS_NAME, "CircleButton_type_prev")
