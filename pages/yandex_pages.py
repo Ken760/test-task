e8c067890bf7a966de19447e5c380579404d1d97
Ken760 <49761163+Ken760@users.noreply.github.com>
2020-12-17 13:50:01 +0300

    Changes

--- pages/yandex_pages.py
+++ pages/yandex_pages.py
@@ -1,7 +1,9 @@
 from selenium.webdriver.common.keys import Keys
 from .base_page import BasePage
 from .locators import YandexSearchLocators
+from .locators import YandexPictureLocators
 import time
+
 
 class TaskPage(BasePage):
     def search_in_yandex(self, word):
@@ -11,3 +13,15 @@ class TaskPage(BasePage):
         assert self.is_element_present(*YandexSearchLocators.SUGGEST), 'Таблица с подсказками отсутствует'
         time.sleep(5)
         search_field.send_keys(Keys.ENTER)
+
+    def go_to_page_images_yandex(self):
+        assert self.is_element_present(*YandexPictureLocators.YANDEX_IMAGE_LINK), "Ссылка на картинки отсутствует"
+        images_link = self.browser.find_element(*YandexPictureLocators.YANDEX_IMAGE_LINK)
+        images_link.click()
+
+    def switch_to_page(self):
+        tabs = self.browser.window_handles  # список
+        browser.switch_to.window(tabs[1])
+
+    def click_images(self):
+        pass
--- tests.py
+++ tests.py
@@ -7,3 +7,7 @@ def test_search_in_yandex(browser):
     yandex_main_page.search_in_yandex('Тензор')
 
 
+def test_yandex_images(browser):
+    yandex_page = TaskPage(browser)
+    yandex_page.go_to_site()
+    yandex_page.go_to_page_images_yandex()
