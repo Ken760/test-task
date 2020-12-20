2bfb64498c841cd8914290fc94d5fa436ad36f24
Ken760 <49761163+Ken760@users.noreply.github.com>
2020-12-20 12:19:25 +0300

    Update Tests.py

--- Tests.py
+++ Tests.py
@@ -9,7 +9,7 @@ def test_search_in_yandex(browser):
     yandex_main_page.go_to_site()
     with allure.step("Проверка наличия поиска"):
         yandex_main_page.search_in_yandex('Тензор')
-    with allure.step("Проверка, что появилась таблица с подсказками (suggest)"):
+    with allure.step("Проверка на то, что появилась таблица с подсказками (suggest)"):
         yandex_main_page.checking_suggest()
     with allure.step("При нажатии Enter появляется таблица результатов поиска"):
         yandex_main_page.click_on_the_search()
@@ -25,14 +25,14 @@ def test_yandex_images(browser):
     with allure.step("Ссылка «Картинки» присутствует на странице"):
         yandex_page.go_to_page_images_yandex()
     yandex_page.switch_to_page()
-    with allure.step("Проверка, что перешли на url https://yandex.ru/images/"):
+    with allure.step("Проверка на то, что перешли на url https://yandex.ru/images/"):
         yandex_page.checking_current_url()
     with allure.step("Открыть категорию, проверить что открылась, в поиске верный текст"):
         yandex_page.click_theme_images()
     with allure.step("Открыть 1 картинку, проверить что открылась"):
         yandex_page.click_images()
-    with allure.step("При нажатии кнопки вперед  картинка изменяется,"
-                     "При нажатии кнопки назад картинка изменяется на тоже изображение"):
+    with allure.step("При нажатии кнопки вперед картинка изменяется,"
+                     "При нажатии кнопки назад картинка изменяется на то же изображение"):
         yandex_page.all_button()
 
 
